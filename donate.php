9e5318b6f2f63e3241f439b9c3827f0c4fdcc3a7
mherrington4330 <65637707+mherrington4330@users.noreply.github.com>
2021-05-06 23:19:41 -0600

    Create donate.php

---
+++ donate.php
@@ -0,0 +1,29 @@
+<?php
+//Code taken from W3 schools, editted by C2C Herrington
+$servername = "PostgreSQL";
+$username = "student";
+$password = "CompSci364";
+$dbname = "WebAppWIIMuseum";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$memID = mysqli_real_escape_string($link, $_REQUEST['memID']);
+$exhibName = mysqli_real_escape_string($link, $_REQUEST['exhibName']);
+$donation = mysqli_real_escape_string($link, $_REQUEST['donation']);
+
+$sql = "INSERT INTO donation (firstname, lastname, email)
+        VALUES ('$memID', '$exhibName', '$donation')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
