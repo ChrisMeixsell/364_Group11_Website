99b371486ec6eca174eb0df42c0d81c977f4bbb0
mherrington4330 <65637707+mherrington4330@users.noreply.github.com>
2021-05-07 01:10:52 -0600

    Update member.php

--- member.php
+++ member.php
@@ -15,14 +15,17 @@ if ($conn->connect_error) {
 $fname = mysqli_real_escape_string($link, $_REQUEST['fname']);
 $lname = mysqli_real_escape_string($link, $_REQUEST['lname']);
 $age = mysqli_real_escape_string($link, $_REQUEST['age']);
-$phone = mysqli_real_escape_string($link, $_REQUEST['phone']);
-$exhibName = mysqli_real_escape_string($link, $_REQUEST['exhibName']);
-$address = mysqli_real_escape_string($link, $_REQUEST['address']);
 $org = mysqli_real_escape_string($link, $_REQUEST['org']);
 $vet = mysqli_real_escape_string($link, $_REQUEST['vet']);
 
-$sql = "INSERT INTO member (fname, lname, age, phone, exhibName, address, org, vet)
-        VALUES ('$fname', '$lname', '$age', '$phone', '$exhibName', '$address', '$org', '$vet')";
+$sql = "INSERT INTO donor (memID, fname, lname, age, totaldonations, org, vet)
+        VALUES (0, '$fname', '$lname', '$age', 0, '$org', '$vet')
+        UPDATE donor
+        SET memID = SELECT FLOOR(RAND()*(99999-1+1))+1
+        WHERE memID = 0
+        UPDATE donor
+        SET totaldonations = SUM(donation.amount)
+        WHERE donation.memID = donor.memID";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
